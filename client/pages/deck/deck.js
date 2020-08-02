61a951453214811de8ce0ac94ff805e46005190f
eugenemesina <eugenemesina21@gmail.com>
2020-08-02 00:10:27 -0700

    Added Collapseable Deck

--- client/pages/deck/deck.js
+++ client/pages/deck/deck.js
@@ -1,7 +1,7 @@
 import FlashCard from './flashcard'
-
+import React, { useState } from 'react';
 export default function Deck(props) {
-
+  const [collapsed,setCollapsed] = useState('true');
   var data = {cards: [
     {FrontTitle: "Question" , FrontContent: "What is Abdominal Aortic Aneurysm", BackTitle: "Answer", BackContent: "An abdominal aortic aneurysm (AAA) is a swelling (aneurysm) of the aorta – the main blood vessel that leads away from the heart, down through the abdomen to the rest of the body." },
     {FrontTitle: "Question" , FrontContent: "What is ACNE", BackTitle: "Answer", BackContent: "Acne is a common skin condition that affects most people at some point. It causes spots, oily skin and sometimes skin that's hot or painful to touch." },
@@ -21,10 +21,8 @@ export default function Deck(props) {
   });
       return (
         <div id = {props.title}>
-          <p>{props.title}</p>
-          <FlashCard FrontTitle = "Poop" FrontContent = "What is Poop" BackTitle = "POOP" BackContent = "POOP POOP POOP"></FlashCard>
-          <FlashCard FrontTitle = "sdaada" FrontContent = "Whatada is Poop" BackTitle = "ada" BackContent = "POOP POOP adaPOOP"></FlashCard>
-          {items}
+           <button onClick= {()=> {setCollapsed(!collapsed)}}>{props.title}</button>
+          {collapsed ? null:items}
         </div>
       );
   }
--- client/pages/decklistspage.js
+++ client/pages/decklistspage.js
@@ -4,8 +4,8 @@ export default function DeckLists(props) {
       return (
         <div>
             <h1> My Deck </h1>
-            <Deck title = "Poop"></Deck>
-            <Deck title = "Poopy"></Deck>
+            <Deck  title = "Poop"></Deck>
+            <Deck  title = "Poopy"></Deck>
         </div>
       );
   }
