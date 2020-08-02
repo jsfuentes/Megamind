1a1b1eb40a8204f9f643e068319f54002daec7eb
Shakthi Visagan <shak360@gmail.com>
2020-08-01 23:11:37 -0700

    more work

--- client/pages/deck/flashcard.js
+++ client/pages/deck/flashcard.js
@@ -1,53 +1,82 @@
 import React, { useReducer } from 'react';
+import combineReducers from 'react-combine-reducers';
 import { Card } from 'react-bootstrap';
 
-const initialState = {
-    side : true
+const sideInitialState = {
+    side: "front"
 }
 
-function init(initialState) {
-    return {
-        side : initialState.side
-    };
+const memdataInitialState = {
+    currentQualityOfResponse: 0 
 }
 
-function reducer(state, action) {
+const sideReducer = (state, action) => {
     switch (action.type) {
         case "flip_to_front":
-            return {
-                side : true
+            return { ...state,
+                side : "front"
             };
         case "flip_to_back":
-            return {
-                side: false
+            return { ...state,
+                side: "back"
             };
         case "reset":
-            return init(action.payload);
-        default:
-            throw new Error();
+            return sideInitialState;
+        default: return state;
     }
 }
+
+const memdataReducer = (state, action) => {
+    console.log(state)
+    switch (action.type) {
+        case 5:
+            return changeMemdataCurrentQualityOfResponse(5, state);
+        case 4:
+            return changeMemdataCurrentQualityOfResponse(4, state);
+        case 3:
+            return changeMemdataCurrentQualityOfResponse(3, state);
+        case 2:
+            return changeMemdataCurrentQualityOfResponse(2, state);
+        case 1:
+            return changeMemdataCurrentQualityOfResponse(1, state);
+        case 0:
+            return changeMemdataCurrentQualityOfResponse(0, state);
+        default: return state;
+        ;
+    }
+}
+
+function changeMemdataCurrentQualityOfResponse(enteredQualityOfResponse, state) {
+    return { ...state,
+        currentQualityOfResponse: enteredQualityOfResponse
+    }
+}
+
+const [rootReducerCombined, initialStateCombined] = combineReducers({
+    side: [sideReducer, sideInitialState],
+    memdata: [memdataReducer,  memdataInitialState]
+});
 
 export default function FlashCard(props){
     // if side == true then the FRONT of the card is shown
     // else, the BACK of the card is shown 
-    const [state, dispatch] = useReducer(reducer, initialState, init);
-
+    const [state, dispatch] = useReducer(rootReducerCombined, initialStateCombined);
+    console.log(state)
     return(
         <>
         <div>
             <Card 
             style={{ "width" : '18rem' }, { "borderStyle" : "solid" }}
             onClick={() => dispatch({
-                type : state.side ? "flip_to_back" : "flip_to_front"
+                type : state.side.side.localeCompare("front")==0 ? "flip_to_back" : "flip_to_front"
             })}
             >
                 <Card.Body>
                     <Card.Title>
-                        {state.side ? props.FrontTitle : props.BackTitle}
+                        {state.side.side.localeCompare("front")==0 ? props.FrontTitle : props.BackTitle}
                     </Card.Title>
                     <Card.Text>
-                        {state.side ? props.FrontContent : props.BackContent}
+                        {state.side.side.localeCompare("front")==0 ? props.FrontContent : props.BackContent}
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -55,7 +84,56 @@ export default function FlashCard(props){
         <div>
         <button 
             onClick={() => dispatch({
-                type: "reset", payload: initialState})}>
+                type: 5
+                })}>
+            Perfect response (5)
+        </button>
+        </div>
+        <div>
+        <button 
+            onClick={() => dispatch({
+                type: 4
+                })}>
+            Correct with some hesitation (4)
+        </button>
+        </div>
+        <div>
+        <button 
+            onClick={() => dispatch({
+                type: 3
+                })}>
+            Correct with great difficulty (3)
+        </button>
+        </div>
+        <div>
+        <button 
+            onClick={() => dispatch({
+                type: 2
+                })}>
+            Incorrect response; remembered after flipping (2)
+        </button>
+        </div>
+        <div>
+        <button 
+            onClick={() => dispatch({
+                type: 1
+                })}>
+            Incorrect response; recognized after flipping (1)
+        </button>
+        </div>
+        <div>
+        <button 
+            onClick={() => dispatch({
+                type: 0
+                })}>
+            Complete blackout (0)
+        </button>
+        </div>
+        <div>
+        <button 
+            onClick={() => dispatch({
+                type: "reset"
+                })}>
             Reset
         </button>
         </div>
