83344636bfdcb3cc680315df6a6454b6322f2c49
eugenemesina <eugenmesina@git>
2020-08-01 18:46:26 -0700

    Create workable flashcard class with dummy dat

--- client/pages/deck/flashcard.js
+++ client/pages/deck/flashcard.js
@@ -1,5 +1,21 @@
-import React, { Component } from 'react'; 
+import React, {useState } from 'react';
+import { Card } from 'react-bootstrap';
+export default function FlashCard(props){
+    const [side, setSide] = useState(false);
 
-class FlashCard extends Component {
-
-}
+    return(
+        <div>
+            <Card onClick={() => setSide(!side)}
+            style={{ width: '18rem'},{"border-style":"solid"}}>
+                <Card.Body>
+                    <Card.Title>
+                        {side ? props.FrontTitle : props.BackTitle}
+                    </Card.Title>
+                    <Card.Text>
+                        {side ? props.FrontContent : props.BackContent}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
