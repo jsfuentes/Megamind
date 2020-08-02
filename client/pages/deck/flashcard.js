03c89d5493fcf8aa49c7ec5a2f0dd0015a6795fd
Shakthi Visagan <shak360@gmail.com>
2020-08-01 20:16:31 -0700

    more work

--- client/pages/deck/flashcard.js
+++ client/pages/deck/flashcard.js
@@ -1,21 +1,49 @@
-import React, {useState } from 'react';
+import React, { useReducer } from 'react';
 import { Card } from 'react-bootstrap';
+
+const initialState = {
+    side : true
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "flip_to_front":
+            return {
+                side : true
+            };
+        case "flip_to_back":
+            return {
+                side: false
+            };
+        default:
+            throw new Error();
+    }
+}
+
 export default function FlashCard(props){
-    const [side, setSide] = useState(false);
+    // if side == true then the FRONT of the card is shown
+    // else, the BACK of the card is shown 
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     return(
+        <>
         <div>
-            <Card onClick={() => setSide(!side)}
-            style={{ width: '18rem'},{"border-style":"solid"}}>
+            <Card 
+            style={{ width: '18rem'}, {"border-style":"solid"}}
+            onClick={() => dispatch({
+                type : state.side ? "flip_to_back" : "flip_to_front"
+            })}
+            >
                 <Card.Body>
                     <Card.Title>
-                        {side ? props.FrontTitle : props.BackTitle}
+                        {state.side ? props.FrontTitle : props.BackTitle}
                     </Card.Title>
                     <Card.Text>
-                        {side ? props.FrontContent : props.BackContent}
+                        {state.side ? props.FrontContent : props.BackContent}
                     </Card.Text>
                 </Card.Body>
             </Card>
         </div>
+        </>
     );
 };
