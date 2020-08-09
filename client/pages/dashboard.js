e9e8305e0b2a5dae091bfe6794483ce8d917cc16
Ryan Koh <ryan_koh@berkeley.edu>
2020-08-08 17:55:50 -0700

    New Deck form

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -6,45 +6,14 @@ import Loading from "../components/Loading";
 import UserContext from "../contexts/UserContext";
 import decks from "../decks.json";
 import axios from 'axios';
-import Modal from 'react-modal';
+import CreateDeck from '../components/CreateDeck';
 import { toast } from "react-toastify";
 const debug = require("debug")("app:Dashboard");
 
 export default function Dashboard(props) {
   const [name, setName] = useState("");
-  const [noEvent, setNoEvent] = useState(true);
-  const [modalIsOpen,setIsOpen] = React.useState(false);
   const { user } = useContext(UserContext);
   const router = useRouter();
-
-  function openModal() {
-    setIsOpen(true);
-  }
- 
-  function closeModal(){
-    setIsOpen(false);
-  }
-
-  function onSubmit(event) {
-      event.preventDefault();
-      axios
-      .post("/api/events", { name, noEvent })
-      .then((resp) => debug("resp recieved", resp));
-      closeModal()
-  }
-
-  const customStyles = {
-    content: {
-      position: 'fixed',
-      top: '50%',
-      left: '50%',
-      width: '50%',
-      color: 'grey',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
-
-
 
   // useEffect(() => {
   //   toast("Must login first");
@@ -74,36 +43,7 @@ export default function Dashboard(props) {
           })}
         </div>
       </div>
-      <button onClick={openModal}>Create New Deck</button>
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          style={customStyles}
-        > 
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={closeModal}>Close</button>
-          
-          <form
-            className="border-4 border-solid rounded-sm flex flex-col justify-center items-center p-6 mb-4"
-            onSubmit={onSubmit}
-          >
-            <div className="text-3xl font-bold">Deck Name</div>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="name"
-              type="text"
-              placeholder="Deck Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            &nbsp;
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Create
-            </button>
-          </form>
-        </Modal>
+      <CreateDeck></CreateDeck>
     </div>
   );
 }
