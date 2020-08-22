3d1e10b05b2dacacec5691beeb8cb288f72784b1
Jorge Fuentes <jsjfuentesj@gmail.com>
2020-08-22 15:06:20 -0700

    Delete client and build

--- client/conf/production.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  NAME: "React Prod",
-  SERVER_URL: "https://slingshow.co",
-  CLIENT_URL: "https://slingshow.co",
-};
--- client/contexts/UserContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const UserContext = React.createContext({
-  user: null,
-  setUser: null,
-});
-
-export default UserContext;
